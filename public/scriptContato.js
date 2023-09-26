f305c250f4d517a691da4abc1d062999949d4556
giovanialmeid <137239551+giovanialmeid@users.noreply.github.com>
2023-09-26 20:10:30 -0300

    deletando o script do public para mudar para a pasta js

--- public/scriptContato.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.querySelector("form");
-  
-    formulario.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      alert("Formulário enviado com sucesso!");
-    });
-  });  
