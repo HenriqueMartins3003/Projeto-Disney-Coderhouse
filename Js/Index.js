360cb29a5cf60e6d7623a45da96c7c20fc193634
Henrique Martins <hiikemartins@gmail.com>
2023-09-28 01:26:03 -0300

    Feat: Final chances on Index page, style and JS

---
+++ Js/Index.js
@@ -0,0 +1,63 @@
+//variaveis da parte de personagens
+const carrosel = document.getElementById("character-carrosel");
+const input = document.getElementById("input-box");
+const button = document.getElementById("button-submit");
+const showContainer = document.getElementById("show-container");
+const listContainer = document.querySelector(".list");
+
+// variaveis do formulario
+const btnSubmitForm = document.getElementById("btnSubmit");
+const emailForm = document.getElementById("emailForm");
+const nomeForm = document.getElementById("nameForm");
+const popUp = document.getElementById("myModal");
+
+//function personagens
+button.addEventListener(
+  "click",
+  (getResult = async () => {
+    if (input.value.trim().length < 1) {
+      alert("O campo de pesquisa não pode ficar vazio!");
+    }
+    showContainer.innerHTML = "";
+    const url = `http://gateway.marvel.com/v1/public/characters?ts=1695869773587&apikey=d67e46f5b331b89af57362e8d0316c90&hash=e3962b05f275713ac68269a9cda8c93e&nameStartsWith=${input.value}`;
+
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    jsonData.data["results"].forEach((element) => {
+      showContainer.innerHTML = `<div class="card-container">
+        <div class="container-character-image">
+            <img src="${
+              element.thumbnail["path"] + "." + element.thumbnail["extension"]
+            }"/>
+        </div>
+        <div class="character-name">${element.name}</div>
+        <div class="character-description">${element.description} </diV>
+        </div>`;
+    });
+  })
+);
+
+window.onload = () => {
+  getResult();
+};
+
+// function do formulario
+
+btnSubmitForm.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!nomeForm.value.length && !emailForm.value.length) {
+    const message = document.createElement("p");
+    message.innerText = "Nome ou Endereço de Email invalidos!";
+    popUp.appendChild(message);
+    popUp.style.display = "block";
+    return;
+  }
+  const message = document.createElement("p");
+  message.innerText = `Parabens ${nomeForm.value} você cadastrou o email: ${emailForm.value} na nossa Newsletter`;
+  popUp.appendChild(message);
+  popUp.style.display = "block";
+});
+
+function closePopUp() {
+  popUp.style.display = "none";
+}
--- Js/categorias.js
+++ Js/categorias.js
@@ -24,31 +24,32 @@ function performSearch() {
   searchResults.innerHTML = "";
 }
 
-  // Perform search and display results
-  if (query.length === 0) {
-    return;
-const findNewsApi = async () => {
-  const url =
-    "https://newsapi.org/v2/everything?q=Disney+&from=2023-09-15&apiKey=c3a8a3012945473c84bfabaa97cd402c";
-  const response = await fetch(url);
-  const newsList = await response.json();
-  return newsList.articles;
-};
+// Perform search and display results
+if (query.length === 0) {
+  return;
 
-const createNewsList = async () => {
-  const news = await findNewsApi();
-  const ulPoint = document.getElementById("listaCategorias");
-  console.log(ulPoint);
+  const findNewsApi = async () => {
+    const url =
+      "https://newsapi.org/v2/everything?q=Disney+&from=2023-09-15&apiKey=c3a8a3012945473c84bfabaa97cd402c";
+    const response = await fetch(url);
+    const newsList = await response.json();
+    return newsList.articles;
+  };
 
-  for (let i = 0; i < 4; i++) {
-    const content = categorias[i].content;
-    const bulletPoint = document.createElement("li");
-    console.log(content);
-    console.log(bulletPoint);
-    bulletPoint.innerText = content;
-    ulPoint.appendChild(bulletPoint);
-  }
-};
+  const createNewsList = async () => {
+    const news = await findNewsApi();
+    const ulPoint = document.getElementById("listaCategorias");
+    console.log(ulPoint);
+
+    for (let i = 0; i < 4; i++) {
+      const content = categorias[i].content;
+      const bulletPoint = document.createElement("li");
+      console.log(content);
+      console.log(bulletPoint);
+      bulletPoint.innerText = content;
+      ulPoint.appendChild(bulletPoint);
+    }
+  };
 
   const pageContent = document.body.textContent.toLowerCase();
 
